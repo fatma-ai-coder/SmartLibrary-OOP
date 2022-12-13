09e66f4fb829b66f361dd9ba8c926786e5835482
Judi | Programmer <75368942+Electronic-mind@users.noreply.github.com>
2022-12-13 13:58:26 +0300

    Login design
    changed the login and added a demo to show how to create frames in the class

--- Login_main2.py
+++ Login_main2.py
@@ -1,68 +1,40 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Login:
     #main page
     def __init__(self, root):
-        self.root = root
-        self.root.geometry("700x500")
-        self.root.title("Login to the Library")
-        self.root.configure(bg='lightgreen')
-        Login_1 = Button(self.root, text="Login", fg="black", font=('arial', 13, 'bold'), command=self.login_interface)
-        Login_1.pack()
+        self.frame = Frame(root, width=150, height=200, bg="white")
+        self.frame.pack(pady=5)
 
 
-    #login_page
-    def login_interface(self):
-        login_root = Toplevel(self.root)
-        login_root.title("Login Page")
-        login_root.geometry("700x500")
-        login_root.configure(bg='lightgreen')
-
-        text1 = Label(login_root, text="Welcome! Please Login", font=('arial', 13))
-        text1.pack()
-        text1.place(x=300, y=5)
+        #login_page
+        self.welcome_text = Label(self.frame, text="Welcome! Please Login", font=('arial', 13), bg="white")
+        self.welcome_text.pack(pady=5)
 
         #Username
-        Username_text = Label(login_root, text="Username :", fg="black", font=('arial', 13, 'bold'))
-        Username_text.pack()
-        Username_text.place(x=250,y=30)
-        self.user_entry = Entry(login_root)
-        self.user_entry.pack()
-        self.user_entry.place(x=330,y=30)
+        self.username_text = Label(self.frame, text="Username :", fg="black", font=('arial', 13, 'bold'), bg='white')
+        self.username_text.pack(pady=5)
+    
+        self.user_entry = Entry(self.frame, width=25, bg='white', font=('arial', 13, 'bold'))
+        self.user_entry.pack(pady=5)
 
         #Password
-        Password_text = Label(login_root, text="Password :", fg="black", font=('arial', 13, 'bold'))
-        Password_text.pack()
-        Password_text.place(x=250, y=60)
-        self.pass_entry = Entry(login_root)
-        self.pass_entry.pack()
-        self.pass_entry.place(x=330, y=55)
+        self.password_text = Label(self.frame, text="Password :", fg="black", font=('arial', 13, 'bold'))
+        self.password_text.pack(pady=5)
+        
+        self.pass_entry = Entry(self.frame, show='*', width=25, bg='white', font=('', 13))
+        self.pass_entry.pack(pady=5)
+        
         #Login_verfy_button
-        Login_Button = Button(login_root, text="Login",fg="black", font=('arial', 13, 'bold'),command=self.login_verfy)
-        Login_Button.pack()
-        Login_Button.place(x=315,y=450)
+        Login_Button = Button(self.frame, text="Login",fg="black", font=('arial', 13, 'bold'),command=self.login_verfy)
+        Login_Button.pack(pady=5)
 
 
     def login_verfy(self):
-        if self.user_entry.get() == "Rahaf858" and self.pass_entry.get() == "12345rgH":
-            s_root = Toplevel()
-            s_root.title("Welcome")
-            s_root.geometry("700x200")
-            s_root.configure(bg='lightgreen')
-            s_text = Label(s_root, text="You have log in Successfully", bg="green", width="500", height="5",
-                          font=("Calibri", 13, "bold"))
-            s_text.pack()
-
+        if self.user_entry.get() == "admin" and self.pass_entry.get() == "admin":
+            messagebox.showinfo("Login", "You have logged in successfully")
+            
         else:
-            f_root = Toplevel()
-            f_root.title("Error")
-            f_root.geometry("700x200")
-            f_root.configure(bg='red')
-            f_text = Label(f_root, text="You have wrote the wrong Username or Password", bg="red", width="500", height="1",
-                          font=("Calibri", 13))
-            f_text.pack()
-
-
-root = Tk()
-object = Login(root)
-root.mainloop()
+            messagebox.showerror("Error", "Login failed")
+    
--- tkinter_demo.py
+++ tkinter_demo.py
@@ -1,16 +1,20 @@
 from tkinter import *
-from tkinter import ttk
-
-SIZE = 200
+from tkinter import messagebox
+from Login_main2 import Login
 
 root = Tk()
 
-frame = ttk.Frame(root, padding = SIZE)
-frame.grid()
-ttk.Label(frame, text = "hello world").grid(column = 0, row = 1)
+window_width = 310
+window_height = 300
 
-ttk.Button(frame, text = "Quit", command= root.destroy).grid(column = 0, row =3)
-ttk.Label(frame, text = "hello", anchor= "center", background= "navy", font= "Helvetica", foreground= "pink").grid(column = 3, row = 1)
-ttk.Scrollbar(frame, orient= "horizontal", takefocus= 100).grid(column =7, row= 9)
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
+x = (screen_width/2) - (window_width/2)
+y = (screen_height/2) - (window_height/2)
+
+root.geometry(f"{window_width}x{window_height}+{int(x)}+{int(y)}")
+root.config(bg="white")
+Login(root)
+root.title("Login")
 root.mainloop()
